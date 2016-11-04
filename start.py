56a4819bca6e373ed65fdcd1d4cb3f04f123b432
Mike Hedges <michael.hedges@socrata.com>
2016-11-04 08:35:32 -0700

    updated levels to read from docs, added bootswatch visual elements

--- start.py
+++ start.py
@@ -1,7 +1,16 @@
+import csv
 from flask import Flask, render_template
 import pdb
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+def get_levels():
+	levels = []
+	with open('docs/levels.csv', 'r') as csvfile:
+		csv_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+		for line in csv_reader:
+			levels.append(line)
+	return levels
 
 @app.route("/")
 def hello():
@@ -9,8 +18,7 @@ def hello():
 
 @app.route("/levelup")
 def levelUp():
-	pdb.set_trace()
-	levels = list(range(1,20))
+	levels = get_levels()
 	return render_template('levelup.html', levels=levels)
 
 if __name__ == "__main__":
