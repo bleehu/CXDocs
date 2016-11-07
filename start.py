bcc861017b8c55687d61b538bb2df5158be721c1
Mike Hedges <michael.hedges@socrata.com>
2016-11-07 09:09:14 -0800

    cleaned up feats more

--- start.py
+++ start.py
@@ -13,10 +13,12 @@ def get_levels():
 	return levels
 	
 def get_feats():
+	feats = []
 	with open("docs/feats.csv") as csvfile:
 		csv_reader = csv.reader(csvfile, delimiter=',',quotechar='"')
 		for line in csv_reader:
 			feats.append(line)
+	return feats
 
 @app.route("/")
 def hello():
@@ -28,19 +30,18 @@ def levelUp():
 	return render_template('levelup.html', levels=levels)
 	
 @app.route("/guns")
-def levelUp():
+def guns():
 	return render_template('guns.html', guns=[])
 	
 @app.route("/armor")
-def levelUp():
-	levels = get_levels()
+def armor():
 	return render_template('armor.html', armors=[])
 
 	
 @app.route("/feats")
-def feats():
+def show_feats():
 	feats = get_feats()
 	return render_template("feats.html", feats=feats)
 
 if __name__ == "__main__":
-    app.run(host = "192.168.0.186")
+    app.run(host = "localhost")
