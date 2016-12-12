89969182f8bee8fb9f5fb2aea0da7f28db5f8a13
Mike Hedges <michael.hedges@socrata.com>
2016-12-12 14:52:53 -0800

    added things to initialize. now deprecated; postgres has a backup tool

--- learning_snippets/initialize postgres.py
+++ learning_snippets/initialize postgres.py
@@ -43,27 +43,27 @@ def postgre_initialize():
 	myCursor.execute("CREATE TYPE role AS ENUM ('ADMIN', 'DM', 'Player')")
 	#initialize tables
 	myCursor.execute("CREATE TABLE players (pk_id int primary key DEFAULT nextval('players_pk_seq'), username text NOT NULL, displayname text NOT NULL, realname text NOT NULL, password text NOT NULL, role role NOT NULL);")
-	myCursor.execute("CREATE TABLE characters (pk_id int primary key DEFAULT nextval('characters_pk_seq'), owner_fk int references players(pk_id) NOT NULL, current_mission int references missions(pk_id), race text NOT NULL, class text NOT NULL, level int NOT NULL);")
+	myCursor.execute("CREATE TABLE characters (pk_id int primary key DEFAULT nextval('characters_pk_seq'), owner_fk int references players(pk_id) NOT NULL, name text NOT NULL, current_mission int references missions(pk_id), race text NOT NULL, class text NOT NULL, level int NOT NULL);")
 	myCursor.execute("CREATE TABLE weapons (pk_id int primary key DEFAULT nextval('weapons_pk_seq'), owner_fk int references characters(pk_id) NOT NULL, name text NOT NULL, manufacturer text, miss int NOT NULL, range int NOT NULL, damage int NOT NULL, clip int NOT NULL, cost int NOT NULL, clip_cost int NOT NULL, notes text);")
 	myCursor.execute("CREATE TABLE weapon_attachments (pk_id int primary key DEFAULT nextval('weapon_attachments_pk_seq'), on_fk int references weapons(pk_id) NOT NULL, name text NOT NULL, effect text NOT NULL);")
 	myCursor.execute("CREATE TABLE armors (pk_id int primary key DEFAULT nextval('armors_pk_seq'), owner_fk int references characters(pk_id) NOT NULL, cost int NOT NULL, coverage int NOT NULL, damage_reduction int NOT NULL, notes text, primary_clips int NOT NULL, secondary_clips int NOT NULL);")
 	myCursor.execute("CREATE TABLE armor_attachments (pk_id int primary key DEFAULT nextval('armor_attachments_pk_seq'), on_fk int references armors(pk_id) NOT NULL, name text NOT NULL, effect text NOT NULL);")
 	myCursor.execute("CREATE TABLE events (pk_id int primary key DEFAULT nextval('events_pk_seq'), mission_fk int references missions(pk_id) NOT NULL, location text, time text);")
-	
+	connection.commit()
 	#fill tables
 	#the player table must be filled first, but we can't fill the table with passwords in plain text.
 	#so you're gonna have to do this manually.
 	
 	#grant searcher permissions to look things up on these tables
-	try:
-		myCursor.execute("CREATE USER searcher PASSWORD 'allDatSQL';")
-	except:
-		print "Could not create searcher. Does the role already exist?"
+	#try:
+		#myCursor.execute("CREATE USER searcher PASSWORD 'allDatSQL';")
+	#except:
+		#print "Could not create searcher. Does the role already exist?"
 	try:
 		myCursor.execute("GRANT SELECT ON characters, weapons, weapon_attachments, armors, armor_attachments, events, missions TO searcher;")
 		myCursor.execute("GRANT SELECT (pk_id, username, displayname) ON players TO searcher;")
 	except:
-		pass
+		print "Could not grant searcher proper permissions. Complain to Michael."
 	
 	connection.commit()
 	
