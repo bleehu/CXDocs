5c5237024ca2e6287f8eebc15f640fadeb3ed949
bleehu <bleehu@uw.edu>
2018-01-04 12:03:58 -0800

    updated enemy cards to have toggling dropdowns instead of onmouseovers

--- static/monster.js
+++ static/monster.js
@@ -4,16 +4,21 @@
     $(document).ready(initialize)
 
     function initialize(){
-        $(".inspectable").hover(showMe, hideMe);
+        $(".inspectable").find(".dropdown-toggle").click(toggleMe);
         $(".inspectable").each(hideMe);
     }
 
     function hideMe(){
-        var myBody = $(this).find(".card-body");
+        var myBody = $(this).parent().find(".card-body");
         $(myBody).collapse('hide');
     }
     function showMe(){
-        var myBody = $(this).find(".card-body");
+        var myBody = $(this).parent().find(".card-body");
         $(myBody).collapse('show');
     }
+    function toggleMe(){
+        var myBody = $(this).parent().parent().find(".card-body");
+        $(myBody).collapse('toggle');
+    }
+
 })();
