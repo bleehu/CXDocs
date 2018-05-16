cf4b002928d0ae2f3a7250fcd80a06e50c16169a
bleehu <bleehu@uw.edu>
2018-05-16 13:54:32 -0700

    added jquery routes

---
+++ static/character_select.js
@@ -0,0 +1,29 @@
+
+//This file uses AJAX to reach out to the github API to retrieve and display news on recent development.
+(function(){
+    $(document).ready(initialize)
+
+    function initialize(){
+        console.log("Starting character select javascript.");
+        
+        $(".deleteButton").click(deleteCharacter); 
+        
+        //$("#saveCharacterButton").click(saveCharacter);
+
+        console.log("done initializing character select javascript.");
+    }
+
+    function deleteCharacter(){
+        var pk_id = 0;
+        $.ajax({
+            url:'/character/modify/' + pk_id,
+            type:'DELETE',
+            success: function(result){
+                //notify of success
+                console.log("Deleted Character with pk_id " + pk_id);
+            }
+        });
+    }
+
+
+})();
