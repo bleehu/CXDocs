103e9fe30634a4dbe82e3277b1f46d67a5fdb441
bleehu <bleehu@uw.edu>
2018-05-21 16:09:28 -0700

    added feat diff and update functions and even more coments

--- characters/feats.py
+++ characters/feats.py
@@ -1,10 +1,12 @@
 import characters_common
+import csv
+import pdb
 import psycopg2
 
 def get_feats():
     connection = characters_common.db_connection()
     myCursor = connection.cursor()
-    myCursor.execute("SELECT feat, prerequisites, description, author,\
+    myCursor.execute("SELECT pk_id, feat, prerequisites, description, author,\
         created_at, private, nanite_cost\
         FROM feats \
         WHERE deleted_at IS NULL;")
@@ -15,16 +17,79 @@ def get_feats():
         feats.append(newFeat)
     return feats
 
+def get_feat_by_id(pk_id):
+    connection = characters_common.db_connection()
+    myCursor = connection.cursor()
+    myCursor.execute("SELECT \
+        pk_id, \
+        feat, \
+        prerequisites,\
+        description, \
+        author, \
+        created_at, \
+        private, \
+        nanite_cost, \
+        FROM feats WHERE pk_id=%s;" % pk_id)
+    myCursor.close()
+    connection.commit()
+    line = myCursor.fetchall[0]
+    my_feat = parse_line_to_feat(line)
+    return my_feat
+
+def update_feat(newFeat):
+    pk_id = newFeat['pk_id']
+    prereqs = str(newFeat['prerequisites']).replace("'",'"')
+
+    featstring = "('%s', \
+     '%s', '%s', '%s', '%s', '%s', '%s')" % \
+     (newFeat['feat'], \
+        prereqs, \
+        newFeat['description'].replace("'", "\\47"),
+        newFeat['author'],
+        newFeat['created_at'],
+        newFeat['private'],
+        newFeat['nanite_cost'])
+    connection = characters_common.db_connection()
+    myCursor = connection.cursor()
+    myCursor.execute("UPDATE feats SET\
+        (feat, \
+        prerequisites,\
+        description, \
+        author, \
+        created_at, \
+        private, \
+        nanite_cost) = %s \
+        WHERE pk_id=%s;" % ( featstring,pk_id))
+    myCursor.close()
+    connection.commit()
+
+
 
 def parse_line_to_feat(line):
     feat = {}
-    feat['feat'] = line[0]
-    feat['name'] = line[0]
-    feat['title'] = line[0]
-    feat['prerequisites'] = line[1]
-    feat['description'] = line[2]
-    feat['author'] = line[3]
-    feat['created_at'] = line[4]
-    feat['private'] = line[5]
-    feat['nanite_cost'] = line[6]
+    feat['pk_id'] = line[0]
+    feat['feat'] = line[1]
+    feat['name'] = line[1]
+    feat['title'] = line[1]
+    if line[2] == "[]":
+        feat['prerequisites'] = []
+    else:
+        prereq_string = line[2][1:-1]
+        the_splits = prereq_string.split(',')
+        prereqs = []
+        for prereq in the_splits:
+            trimmed = prereq.strip()[1:-1]
+            prereqs.append(trimmed)
+        feat['prerequisites'] = prereqs
+    feat['description'] = line[3]
+    feat['author'] = line[4]
+    feat['created_at'] = line[5]
+    feat['private'] = line[6]
+    feat['nanite_cost'] = line[7]
     return feat
+
+def parse_prereqs(prereq_string):
+    prereqs = []
+    parser = csv.reader(prereq_string, delimiter=",", quotechar='"')
+
+    return prereqs
--- characters/update_feats_table.py
+++ characters/update_feats_table.py
@@ -26,12 +26,23 @@ def update_feats():
     psql_feat_map = {}
     for feat in psql_feats:
         psql_feat_map[feat['feat']] = feat
+    doc_feat_map = {}
     for feat in doc_feats:
-        if feat['title'] not in psql_feat_map.keys():
+        doc_feat_map[feat['feat']] = feat
+        if feat['feat'] not in psql_feat_map.keys():
             add_feat(feat)
+        elif diff_feat(feat, psql_feat_map[feat['feat']]):
+            #if there is a matching feat in the database, but it's different
+            update_a_feat(feat, psql_feat_map[feat['feat']])
+    print "All done!"
+    pdb.set_trace()
 
 
-""""""
+
+
+""" read the feats from the plain text file. 
+Expected feat_filepath to be an absolute filepath to the Compound_X
+feats document. """
 def ingest_feats_doc(feat_filepath):
     feats = []
     lines = []
@@ -54,7 +65,7 @@ def ingest_feats_doc(feat_filepath):
             index_of_last_title = index
     print "Collected doc feats"
     for feat in feats:
-        print "%s\t%s\t%s" % (feat['title'], feat['nanite cost'], feat['prerequisites'])
+        print "%s\t%s\t%s" % (feat['feat'], feat['nanite_cost'], feat['prerequisites'])
 
     return feats
 
@@ -65,12 +76,12 @@ def parse_a_feat(lines):
         start_index = lines[0].index('[')
         end_index = lines[0].index('Nanites]')
         nanite_cost = lines[0][start_index + 1:end_index].strip()
-        this_feat['nanite cost'] = nanite_cost
+        this_feat['nanite_cost'] = nanite_cost
         title = lines[0][2:start_index].strip()
-        this_feat['title'] = title
+        this_feat['feat'] = title
     except:
-        this_feat['nanite cost'] = 0
-        this_feat['title'] = lines[0].strip()[2:-2]
+        this_feat['nanite_cost'] = 0
+        this_feat['feat'] = lines[0].strip()[2:-2]
     #find and parse prerequisites
     end_of_prereqs = 0
     prereq_flag = False
@@ -102,17 +113,22 @@ with the configs 'characters_psql_pass' and 'characters_psql_user'
 options set. 
 uses psycopg2 to insert a new feat into the database """
 def add_feat(feat):
-    print "This feat isn't currently in the database."
-    print "title: %s" % feat['title']
-    print "description: %s" % feat['description']
+    print '\n\n\n'
+    print "\033[94m" + "This feat isn't currently in the database." + '\033[0m'
+    print "Title: %s" % feat['feat']
+    print "Nanite Cost: %s" % feat['nanite_cost']
+    print "Prerequisites: %s " % str(feat['prerequisites'])
+    print "Description: %s" % feat['description']
     print ""
     answer = yes_or_no("Should we add it? (y/n)")
     if answer:
-        title = feat['title'].replace("'", "%39")
+        title = feat['feat'].replace("'", "%39")
         description = feat['description'].replace("'","%39")
+        prereqs = str(feat['prerequisites']).replace("'",'"')
+        prereqs = prereqs.replace(',', '\\54')
         featstring = "('%s', '%s', '%s', 'feat_parser', now(), '%s', 'f')" % \
-        (title, description, feat['nanite cost'], \
-            str(feat['prerequisites']).replace("'",'"'))
+        (title, description, feat['nanite_cost'], \
+            prereqs)
         connection = characters_common.db_connection()
         cursor = connection.cursor()
         cursor.execute("INSERT INTO feats (feat, description, nanite_cost, \
@@ -120,6 +136,34 @@ def add_feat(feat):
             VALUES %s;" % featstring)
         cursor.close()
         connection.commit()
+
+""" prompt to change a feat in the database to match what's on the page """
+def update_a_feat(doc_feat, psql_feat):
+    print '\n\n\n'
+    print '\033[94mThe feat "%s" is different in the database than it is on paper.\033[0m' % doc_feat['feat']
+    print ''
+    print "\033[91m(old) Database feat:\033[0m"
+    print "%s" % psql_feat['feat']
+    print "Nanite Cost: %s" % psql_feat['nanite_cost']
+    print "Prerequisites:"
+    print psql_feat['prerequisites']
+    print psql_feat['description']
+    print ''
+    print "\033[92m(new) Docs feat:\033[0m"
+    print "%s" % doc_feat['feat']
+    print "Nanite Cost: %s" % doc_feat['nanite_cost']
+    print "Prerequisites:"
+    print doc_feat['prerequisites']
+    print doc_feat['description']
+    print ''
+    answer =  yes_or_no('Would you like to update to the new feat? (y/n)')
+    if answer:
+        doc_feat['pk_id'] = psql_feat['pk_id']
+        doc_feat['created_at'] = psql_feat['created_at']
+        doc_feat['private'] = psql_feat['private']
+        doc_feat['author'] = "feat_parser"
+        feats.update_feat(doc_feat)
+
 
 """ Expects feat_1 and feat_2 to be maps representing feats in the
 psql database for CXDocs. Each map should have the following keys:
@@ -132,24 +176,25 @@ nanite cost and description.
 Returns True otherwise. 
 Raises exceptions if the feats passed do not match the standard format."""
 def diff_feat(feat_1, feat_2):
-    if "title" not in feat_1.keys():
+    if "feat" not in feat_1.keys():
         print "Error! feat_1 has no name! Feat: %s" % str(feat_1)
         raise Exception
-    if "title" not in feat_2.keys():
+    if "feat" not in feat_2.keys():
         print "Error! feat_2 has no name! Feat: %s" % str(feat_2)
         raise Exception
     if "description" not in feat_1.keys():
-        print "Error! feat_1 has no description! Feat: %s" feat_1['title']
+        print "Error! feat_1 has no description! Feat: %s" % feat_1['feat']
         raise Exception
+    pdb.set_trace()
     if "description" not in feat_2.keys():
-        print "Error! feat_2 has no description! Feat: %s" % feat_2['title']
-    if feat_1['title'].strip() != feat_2['title'].strip():
+        print "Error! feat_2 has no description! Feat: %s" % feat_2['feat']
+    if feat_1['feat'].strip() != feat_2['feat'].strip():
         return True 
     if feat_1['description'].strip() != feat_2['description'].strip():
         return True
-    if sorted(feat_1['prerequisites']) != sorted(feat_2['prerequisites'])
+    if sorted(feat_1['prerequisites']) != sorted(feat_2['prerequisites']):
         return True
-    if feat_1['nanite cost'].strip() != feat_2['nanite cost'].strip():
+    if str(feat_1['nanite_cost']).strip() != str(feat_2['nanite_cost']).strip():
         return True
     return False
 
