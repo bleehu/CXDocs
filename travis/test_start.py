2a6d0471543d5530f300220b72f94bd1234fb7c6
Bleehu <bleehu@uw.edu>
2018-09-23 16:26:16 -0700

    added testing for login systems 	modified:   travis/test_start.py

--- travis/test_start.py
+++ travis/test_start.py
@@ -1,7 +1,6 @@
 import pytest #pytest helps us create the testbench and clean up afterwards
-
-from ..server import app as cxApp 
-
+from flask import session
+from ..server import app as cxApp
 
 #the pytest fixtures are a way that the pytest tools keep track of encapsulated
 # components.
@@ -10,13 +9,15 @@ def app():
     #we use the same create_app method that the flask tool expects to call when
     #initially starting up.
     tap = cxApp.create_app()
+    cxApp.debug = False
+    cxApp.testing = True
     #pytest uses a yield to deque the number of test apps it needs.
     yield tap
 
 
 @pytest.fixture
 def client(app):
-    #each client keeps track of a session; think of it like one user with one
+    #each client keeps track of a sessionion; think of it like one user with one
     # tab open. A test_client is an object defined in Flask.
     client = app.test_client()
     yield client
@@ -27,4 +28,21 @@ def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
 
-#TODO: add tests for logging in and out.
+def test_login(client):
+    valid = {"uname":"travisTest", 
+        "password":"travis1Tractor"}
+    invalid = {"usname":"bungleface", 
+        "password":"fairyable"}
+    with client as c:
+        response = client.post("/login", data=valid, follow_redirects=True)
+        assert session['username'] == "travisTest"
+        assert session['displayname'] == "travis_test"
+        assert session['role'] == "GM"
+        response = client.post("/logout", follow_redirects=True)
+        assert 'username' not in session
+        assert 'displayname' not in session
+        assert 'role' not in session
+        response = client.post("/login", data=invalid, follow_redirects=True)
+        assert session['username'] == None
+        assert session['displayname'] == None
+        assert session['role'] == None
