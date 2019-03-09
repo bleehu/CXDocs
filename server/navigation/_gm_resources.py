6afb481614884b63601afc124e88af429e300984
Austin King <aust.a.king@gmail.com>
2019-03-08 18:31:01 -0800

    Edited navigation module to hopefully make it more manageable, including... - Changed names to be more accurate/meaningful (such as file names, functions, and parameters) - Separated dict files more to work within categories (this will likely change in the future, as it should ideally, if we can manage it, closely reflect how the site is navigated) - Created navbar tuples for more DRY code and help determine what navigation should look and act like (looking to make this even more DRY in the future) - Added a couple paths to theoretical pages (right now they use the home.html template; they can be easily changed to use new templates or docs) - Added a function to help aggregate the nav dict so paths can be obtained via explicit dicts and the config filepath options

---
+++ server/navigation/_gm_resources.py
@@ -0,0 +1,34 @@
+# Need to manually create a dictionary to link the config filepaths to routes with labels and nav lists we want associated with each other.
+# (Having to manage this is why the module is broken down into these files.)
+
+_gm_navbar = ( '/rules/', '/rules/glossary', '/gm/' )
+
+def get_dict():
+    return {
+        # Paths should match options in the Parser section of the config file
+        # Each path key must have a tuple containing values IN THE FOLLOWING ORDER:
+            # a label string (this will be seen by users (keep it brief))
+            # a route string (this is where the link will take a user to; include full route for now with all slashes)
+            # a navbar tuple (NO MORE THAN 4 VALUES; the order listed will be the order displayed on the navbar)
+
+            # (optional) 4th value: a nav_list tuple, listing the options that would appear under its navbar label (usually to very specific pages)
+
+        '/gm/designhowto': {
+            'label': "Designing Campaigns",
+            'navbar': _gm_navbar,
+            'template_to_render': 'creation_manuals/design_how_to.html',
+            'nav_list': None
+        },
+        '/gm/monsterweaponshowto': {
+            'label': "Designing Enemy Weapons",
+            'navbar': _gm_navbar,
+            'template_to_render': 'creation_manuals/monster_weapon_how_to.html',
+            'nav_list': None
+        },
+        '/gm/monsterarmorhowto': {
+            'label': "Designing Enemy Armor",
+            'navbar': _gm_navbar,
+            'template_to_render': 'creation_manuals/monster_armor_how_to.html',
+            'nav_list': None
+        }
+    }
