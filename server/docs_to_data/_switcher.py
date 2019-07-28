a3765f97c578219d7f336efd36c00bfeec93bc7a
Austin King <aust.a.king@gmail.com>
2019-07-27 21:50:20 -0700

    Got (Un)Ordered Lists Working Note: A blank line must be present after the last item of a list in order to work properly.

--- server/docs_to_data/_switcher.py
+++ server/docs_to_data/_switcher.py
@@ -3,13 +3,13 @@ import re
 _re_str = r'('                                  # G0 START - Create group 0
 _re_str += r'^( {4}|\t)'                        # Check for paragraphs (4 spaces or a tab) at start of line
 _re_str += r'|^\s*('                            # G1 START - Check for other patterns at start of line that don't rely on whitespace
-_re_str += r'\={30}|\={5}|\={2}'                # Headings patterns (chapter, section, subsection) (repetitive equals: 30, 4, and 2)
+_re_str += r'\={30}|\={5}|\={2}'                # Headings patterns (chapter, section, subsection)
 _re_str += r'|[\*-]\s'                          # Unordered List item
 _re_str += r'|\*{1,2}'                          # Footnotes (footnote or long_footnote)
 _re_str += r'|\+'                               # Table corner
 _re_str += r'|\|'                               # Table data row
 _re_str += r')'                                 # G1 END - Close group looking at start of line
-_re_str += r'|\s*((?<=\d)|(?<=\d\d))\.(?=\s)'   # Ordered List patterns (lookahead has strict rules; lists shouldn't go over 99 anyway)
+_re_str += r'|((?<=^\d)|(?<=^\d\d))\.(?=\s)'   # Ordered List patterns (lookbehind has strict rules; lists shouldn't go over 99 anyway)
 _re_str += r'|\:$|\:\s'                         # Colon patterns (topic and definition)
 _re_str += r')'                                 # G0 END - Close group 0
 
@@ -47,7 +47,8 @@ T_SWITCHER = {
         'table': 'table',
         'table_row': 'tr',
         'table_data': 'td',
-        'ordered_list': 'ul'
+        'ordered_list': 'ol',
+        'list_item': 'li'
     },
 
     'IS_TYPE_MULTILINE': {
@@ -56,13 +57,14 @@ T_SWITCHER = {
         'subsection': False,
         'topic': False,
         'paragraph': True,
-        'unordered_list': False,
+        'unordered_list': True,
         'footnote': True,           # Assume true (because long can exist)
         'definition_list': False,
         'table': True,
         'table_row': False,
         'table_data': False,
-        'ordered_list': False
+        'ordered_list': True,
+        'list_item': False
     }
 }
 
@@ -83,7 +85,7 @@ class Token:
 
         if T_SWITCHER['IS_TYPE_MULTILINE'].get(tkn_type):
             print('{} is multiline'.format(self.type))
-            self.incomplete = True  # self.new --> del self.new
+            self.incomplete = True
 
         # print("SUCCESS!")
 
@@ -112,11 +114,16 @@ class Token:
 
 def create_token(tkn_type, content):
     # Add content
-    new_token = Token(tkn_type, content)
+    if tkn_type == 'ordered_list' or tkn_type == 'unordered_list':
+        # List items will be children of a list group's token
+        new_token = Token(tkn_type)
+        new_token.children.append(Token('list_item', content))
+    else:
+        new_token = Token(tkn_type, content)
 
-    # For definition item, add definition as child to term
-    if tkn_type == 'definition_list':
-        new_token.children.append(Token(tkn_type, extract_content_from_line(text_line.lstrip(), content + ': ')))
+        # For definition item, add definition as child to term
+        if tkn_type == 'definition_list':
+            new_token.children.append(Token(tkn_type, extract_content_from_line(text_line.lstrip(), content + ': ')))
 
     print('NEW {}'.format(new_token.type))
     return new_token
@@ -127,11 +134,12 @@ def extract_content_from_line(text, match, tkn_type):
     else:
         content_list = text.split(match, 1)
 
-    content_list.remove('')
-    # print('extract {}'.format(content_list))
-
     if tkn_type == 'ordered_list':
         content_list.pop(0)     # Pop number from ordered item
+    else:
+        content_list.remove('')
+
+    print('extract {}'.format(content_list))
 
     return content_list[0].strip()
 
@@ -151,14 +159,15 @@ def process_line(token, text_line):
 
     # GET MATCH; DETERMINE TYPE
     match = re.search(TOKENS_REGEX, text_line)
-    # print('MATCH::: {}'.format(match))
+    print('MATCH::: <{}>'.format(match))
 
     if match != None:
         match = match.group(0)
 
-    if match == None:
+
+    if match == None or (token != None and token.type == 'paragraph' and not token.is_complete()):
         # print('No match; token: {}'.format(token.type))
-        # No match means raw content; add it to latest token if allowed
+        # No match or in the middle of a paragraph means raw content; add it to latest token if allowed
         if token != None and not token.is_complete():
             print('Adding content... {}'.format(text_line.lstrip()))
             token.add_content(text_line.lstrip())
@@ -189,6 +198,12 @@ def process_line(token, text_line):
     if (tkn_type == 'table_row' or tkn_type == 'table') and token != None:
         if token.type == 'table':
             return update_table(token, content)
+
+    # Add list item if latest token is incomplete list group
+    ul_or_ol = 'ordered_list'   # Conditional substring for both unordered and ordered lists ("_list" would also get definition lists)
+    if ul_or_ol in tkn_type and ul_or_ol in token.type and not token.is_complete():
+        token.children.append(Token('list_item', content))
+        return token
 
     # Last option: create a token
     return create_token(tkn_type, content)
